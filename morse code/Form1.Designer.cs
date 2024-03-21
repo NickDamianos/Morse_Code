f2a0ab00b1ac4408f07ec3d16fc499024b3bdde6
Nick Damianos <nikolaos_damianos_1907@hotmail.com>
2024-03-22 01:56:11 +0200

    first commit

---
+++ morse code/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace morse_code
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.textBox1.Location = new System.Drawing.Point(4, 79);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(285, 100);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(177, 199);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(63, 53);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Image = global::morse_code.Properties.Resources._1489608662_Play1Normal;
+            this.button2.Location = new System.Drawing.Point(53, 199);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(63, 53);
+            this.button2.TabIndex = 3;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.textBox2.Location = new System.Drawing.Point(4, 271);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(285, 125);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1, 419);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Ⓒ Νικόλαος Δαμιανός";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(304, 435);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Morse Code";
+            this.Theme = MetroFramework.MetroThemeStyle.Dark;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ morse code/Form1.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech;
+using System.Speech.Recognition;
+using System.Threading;
+
+namespace morse_code
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        /*
+        public SpeechRecognitionEngine eng;
+        public Grammar g;
+        public Thread RecThread;
+        public Boolean RecognizerState = true;
+        */
+        public Form1()
+        {
+            InitializeComponent();
+            label1.ForeColor = Color.Red;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            
+            Console.Beep(800, 100);
+        }
+
+        private void sound(string morse) {
+            int Hz = 800;
+            int ms = 400;
+            string tmp = morse[0].ToString();
+            for (int i = 1; i < morse.Length; i++) {
+                if (morse[i] == '-' && morse[i - 1] == '.') {
+                    tmp += " ";
+                }
+                tmp += morse[i];
+            }
+            for (int i = 0; i < tmp.Length; i++) {
+                if (tmp[i] == '.')
+                {
+                    Console.Beep();
+                }
+                else if (tmp[i] == '-')
+                {
+                    Console.Beep(Hz, ms);
+                }
+                else {
+                    System.Threading.Thread.Sleep(25);
+                    continue;
+                }
+                System.Threading.Thread.Sleep(5);
+            }
+            System.Threading.Thread.Sleep(250);
+        }
+
+        private string morse(char gg) {
+            string mrs = "";
+            if (gg == 'A' || gg == 'a')
+            {
+                mrs = ".-";
+            }
+            else if (gg == 'B' || gg == 'b')
+            {
+                mrs = "-...";
+            }
+            else if (gg == 'C' || gg == 'c')
+            {
+                mrs = "-.-.";
+            }
+            else if (gg == 'D' || gg == 'd')
+            {
+                mrs = "-..";
+            }
+            else if (gg == 'E' || gg == 'e')
+            {
+                mrs = ".";
+            }
+            else if (gg == 'F' || gg == 'f')
+            {
+                mrs = "..-.";
+            }
+            else if (gg == 'G' || gg == 'g')
+            {
+                mrs = "--.";
+            }
+            else if (gg == 'H' || gg == 'h')
+            {
+                mrs = "....";
+            }
+            else if (gg == 'I' || gg == 'i')
+            {
+                mrs = "..";
+            }
+            else if (gg == 'J' || gg == 'j')
+            {
+                mrs = ".---";
+            }
+            else if (gg == 'K' || gg == 'k')
+            {
+                mrs = "-.-";
+            }
+            else if (gg == 'L' || gg == 'l')
+            {
+                mrs = ".-..";
+            }
+            else if (gg == 'M' || gg == 'm')
+            {
+                mrs = "--";
+            }
+            else if (gg == 'N' || gg == 'n')
+            {
+                mrs = "-.";
+            }
+            else if (gg == 'O' || gg == 'o')
+            {
+                mrs = "---";
+            }
+            else if (gg == 'P' || gg == 'p')
+            {
+                mrs = ".--.";
+            }
+            else if (gg == 'Q' || gg == 'q')
+            {
+                mrs = "--.-";
+            }
+            else if (gg == 'R' || gg == 'r')
+            {
+                mrs = ".-.";
+            }
+            else if (gg == 'S' || gg == 's')
+            {
+                mrs = "...";
+            }
+            else if (gg == 'T' || gg == 't')
+            {
+                mrs = "-";
+            }
+            else if (gg == 'U' || gg == 'u')
+            {
+                mrs = "..-";
+            }
+            else if (gg == 'V' || gg == 'v')
+            {
+                mrs = "...-";
+            }
+            else if (gg == 'W' || gg == 'w')
+            {
+                mrs = ".--";
+            }
+            else if (gg == 'X' || gg == 'x')
+            {
+                mrs = "-..-";
+            }
+            else if (gg == 'Y' || gg == 'y')
+            {
+                mrs = "-.--";
+            }
+            else if (gg == 'Z' || gg == 'z')
+            {
+                mrs = "--..";
+            }
+            else if (gg == '0')
+            {
+                mrs = "-----";
+            }
+            else if (gg == '1')
+            {
+                mrs = ".----";
+            }
+            else if (gg == '2')
+            {
+                mrs = "..---";
+            }
+            else if (gg == '3')
+            {
+                mrs = "...--";
+            }
+            else if (gg == '4')
+            {
+                mrs = "....-";
+            }
+            else if (gg == '5')
+            {
+                mrs = ".....";
+            }
+            else if (gg == '6')
+            {
+                mrs = "-....";
+            }
+            else if (gg == '7')
+            {
+                mrs = "--...";
+            }
+            else if (gg == '8')
+            {
+                mrs = "---..";
+            }
+            else if (gg == '9')
+            {
+                mrs = "----.";
+            }
+            else if (gg == ' ') {
+                mrs = " ";
+            }
+
+            return mrs;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string tmp = "";
+            for (int i = 0; i < textBox2.Text.Length; i++) {
+                for (int j = i; textBox2.Text[i] != ' '; i++) {
+                    tmp += textBox2.Text[i];
+                }
+
+                sound(tmp);
+                tmp = " ";
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //Console.Beep(800, 100);
+            textBox2.Text = "";
+            for (int i = 0; i < textBox1.Text.Length; i++)
+            {
+                textBox2.Text += morse(textBox1.Text[i]) + " ";
+            }
+        }
+       
+        private void Form1_Load(object sender, EventArgs e)
+        { /*
+            GrammarBuilder build = new GrammarBuilder();
+            build.AppendDictation();
+
+            // eng = new SpeechRecognitionEngine();
+            // eng.SetInputToDefaultAudioDevice();
+            g = new Grammar(build);
+            eng.SetInputToDefaultAudioDevice();
+
+            eng.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(eng_rec);
+
+            //Here will initialize the recognizer thread:
+
+            RecognizerState = true;
+            RecThread = new Thread(new ThreadStart(RecThreadFunction));
+            RecThread.Start();*/
+        }
+        
+        private void button3_Click(object sender, EventArgs e)
+        {
+           // RecognizerState = true;
+        }
+        
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //RecognizerState = false;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
